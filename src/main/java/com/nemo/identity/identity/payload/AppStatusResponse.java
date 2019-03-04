7f7a7255aa784649643e17eedfa1aeab587b7b4f
Dmitry Kartsev <dkartsev@redhat.com>
2019-03-04 09:58:36 +0200

    add hostname to identity status method

--- src/main/java/com/nemo/identity/identity/payload/AppStatusResponse.java
+++ src/main/java/com/nemo/identity/identity/payload/AppStatusResponse.java
@@ -3,6 +3,8 @@ package com.nemo.identity.identity.payload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.Instant;
 
 public class AppStatusResponse {
@@ -12,6 +14,7 @@ public class AppStatusResponse {
     private Instant time;
     private String artifact;
     private String group;
+    private String hostname;
 
     public AppStatusResponse(BuildProperties buildProperties) {
         this.name = buildProperties.getName();
@@ -19,6 +22,11 @@ public class AppStatusResponse {
         this.time = buildProperties.getTime();
         this.artifact = buildProperties.getArtifact();
         this.group = buildProperties.getGroup();
+        try {
+            this.hostname = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
 
